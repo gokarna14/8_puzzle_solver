d20127241c194cc748e489d781d223f2c96d3d8a
System Administrator <gokarna.adhikari14@gmail.com>
2022-07-25 21:19:32 +0545

    Slight change

--- solver.py
+++ solver.py
@@ -153,5 +153,7 @@ background_colour = (255,255,255)
 screen = pygame. display. set_mode((width, height))
 pygame. display. set_caption('Tutorial 1')
 screen. fill(background_colour)
+
+
 pygame. display. flip()
 running = True
